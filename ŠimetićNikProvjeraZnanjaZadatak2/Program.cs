986ffc8f28806b15140ed2ff63036331b96eb880
niksimetic <55134764+niksimetic@users.noreply.github.com>
2020-03-03 12:54:58 +0100

    Add files via upload

---
+++ ŠimetićNikProvjeraZnanjaZadatak2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ŠimetićNikProvjeraZnanjaZadatak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unos;
+            int a;
+            int b;
+            
+
+            Console.WriteLine("Upisite nekoliko znakova: ");
+            unos = Console.ReadLine();
+            a = Convert.ToInt32(unos.Substring(0,3));
+            b = Convert.ToInt32(unos.Substring(unos.Length -3, 3));
+            if (unos.Length < 7) {
+                Console.WriteLine("Upisali ste:" + unos);
+            }
+            else
+            {
+                Console.WriteLine("Ispis je:" + a + b );
+                }
+            Console.ReadKey();
+            }
+            
+         }
+    }
+
